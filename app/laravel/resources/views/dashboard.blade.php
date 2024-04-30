8a7d0ebc277820b5f1e6645a6dff82b5c8a7d23b
shovakmisha <shovakmisha93@gmail.com>
2024-04-30 15:49:54 +0300

    select data from db by DB Facade

--- app/laravel/resources/views/dashboard.blade.php
+++ app/laravel/resources/views/dashboard.blade.php
@@ -25,7 +25,7 @@
                                 <th scope="row">{{ $user->id }}</th>
                                 <td>{{ $user->name }}</td>
                                 <td>{{ $user->email }}</td>
-                                <td>{{ $user->created_at }}</td>
+                                <td>{{ Carbon\Carbon::parse($user->created_at)->diffForHumans() }}</td>
                             </tr>
                         @endforeach
                     </tbody>
--- app/laravel/routes/web.php
+++ app/laravel/routes/web.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
-use App\Models\User;
+// use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -36,7 +37,8 @@ Route::middleware([
 ])->group(function () {
     Route::get('/dashboard', function () {
 
-        $users = User::all();
+        // $users = User::all();
+        $users = DB::table('users')->get();
 
         return view('dashboard', compact('users'));
     })->name('dashboard');
